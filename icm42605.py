390996be84ede84110a14ee200e50915f3a70dca
Witherflare <80921641+Witherflare@users.noreply.github.com>
2023-08-09 17:08:01 -0400

    Fixed merge

--- icm42605.py
+++ icm42605.py
@@ -1,3 +1,5 @@
+import machine
+
 # Registers
 DEVICE_CONFIG = 0x11
 FIFO_CONFIG   = 0x16
@@ -27,35 +29,68 @@ class ICM42605:
         self.i2c = i2c
         self.addr = address
         self.operating = False
-        self.x = 0
-        self.x_bias = 0
+        self.gx = 0
+        self.gy = 0
+        self.gz = 0
+        self.gRes = 2000/32768
+        self.gOdr = 1000
+        self.aRes = 16/32768
         
-    def config_gyro(self, value="b'\x48'"):
+    def config_gyro(self, value=b'\x48'):
         if self.operating:
             self.disable()
         self.i2c.writeto_mem(self.addr, GYRO_CONFIG, value)
+        gyro_fs_sel = int.from_bytes(value, 'big') & 0xE0
+        if gyro_fs_sel == 0x40:
+            self.gRes = 500/32768
+            
+        gyro_odr = int.from_bytes(value, 'big') & 0x0F
+        if gyro_odr == 0x08:
+            self.gOdr = 100
+            
         
     def get_bias(self):
         print("Keep the board flat and still while the gyroscope is calibrated.")
+        time.sleep(5)
             
-        precision = 100 # change this number to change precision of calibration. higher number will take longer 
+        precision = 100 # change this number to change precision of calibration. higher number will take longer
+        self.ax_bias = 0
+        self.ay_bias = 0
+        self.az_bias = 0
+        self.gx_bias = 0
+        self.gy_bias = 0
+        self.gz_bias = 0
         
-        # X
-        x_result = []
         for i in range(precision):
-            x_result.append(self.get_gyro_x())
+            all_values = i2c.readfrom_mem(self.addr, TEMP_DATA1, 14)
+            val = (all_values[2] << 8) | all_values[3]
+            self.ax_bias += ((val - 0x10000) if (val & 0x8000) else val) * self.aRes
+            val = (all_values[4] << 8) | all_values[5]
+            self.ay_bias += ((val - 0x10000) if (val & 0x8000) else val) * self.aRes
+            val = (all_values[6] << 8) | all_values[7]
+            self.az_bias += ((val - 0x10000) if (val & 0x8000) else val) * self.aRes
+            val = (all_values[8] << 8) | all_values[9]
+            self.gx_bias += ((val - 0x10000) if (val & 0x8000) else val) * self.gRes
+            val = (all_values[10] << 8) | all_values[11]
+            self.gy_bias += ((val - 0x10000) if (val & 0x8000) else val) * self.gRes
+            val = (all_values[12] << 8) | all_values[13]
+            self.gz_bias += ((val - 0x10000) if (val & 0x8000) else val) * self.gRes
+            time.sleep(0.05)
         
-        x_err = sum(x_result) / len(x_result)
-        self.x_bias = x_err
-        
+        self.ax_bias /= precision
+        self.ay_bias /= precision
+        self.az_bias /= precision
+        self.gx_bias /= precision
+        self.gy_bias /= precision
+        self.gz_bias /= precision
         
     def enable(self, accel=True, gyro=True, temp=True):
         if self.operating:
             return False
         
         # Start FIFO
-        self.i2c.writeto_mem(self.addr, FIFO_CONFIG1, b'\x02')
-        self.i2c.writeto_mem(self.addr, FIFO_CONFIG, b'\x40')
+        #self.i2c.writeto_mem(self.addr, FIFO_CONFIG1, b'\x02')
+        #self.i2c.writeto_mem(self.addr, FIFO_CONFIG, b'\x40')
         
         val = 0x00
         if accel:
@@ -78,7 +113,29 @@ class ICM42605:
         if gyro_value & 0x8000:
             gyro_value -= 0x10000
     
-        return gyro_value * (500.0 / 32768.0 / 128)
+        return gyro_value * self.gRes
+    
+    def get_accel_and_gyro_data(self):
+        data = self.i2c.readfrom_mem(self.addr, ACCEL_DATA_X1, 12)
+        val = (data[0] << 8) | data[1]
+        ax = ((val - 0x10000) if (val & 0x8000) else val) * self.aRes - self.ax_bias
+        val = (data[2] << 8) | data[3]
+        ay = ((val - 0x10000) if (val & 0x8000) else val) * self.aRes - self.ay_bias
+        val = (data[4] << 8) | data[5]
+        az = ((val - 0x10000) if (val & 0x8000) else val) * self.aRes - self.az_bias
+        val = (data[6] << 8) | data[7]
+        gx = ((val - 0x10000) if (val & 0x8000) else val) * self.gRes - self.gx_bias
+        val = (data[8] << 8) | data[9]
+        gy = ((val - 0x10000) if (val & 0x8000) else val) * self.gRes - self.gy_bias
+        val = (data[10] << 8) | data[11]
+        gz = ((val - 0x10000) if (val & 0x8000) else val) * self.gRes - self.gz_bias
+        return (ax, ay, az, gx, gy, gz)
+    
+    def updateData(self, time):
+        data = self.get_accel_and_gyro_data()
+        self.gx += data[3] * time/1000
+        self.gy += data[4] * time/1000
+        self.gz += data[5] * time/1000
     
     def fifo_count(self):
         count = self.i2c.readfrom_mem(self.addr, FIFO_COUNTH, 2)
@@ -91,10 +148,12 @@ class ICM42605:
             data = int.from_bytes(self.i2c.readfrom_mem(self.addr, FIFO_DATA, 1), 'big')
             if ( data & 0xE0 == 0x20): # Contains data, not accel, yes gyro
                 gyr = self.i2c.readfrom_mem(self.addr, FIFO_DATA, 8)
-                gyro_value = (gyr[1] << 8) | gyr[2]
-                if gyro_value & 0x8000:
-                    gyro_value -= 0x10000
-                self.x += gyro_value * (500.0 / 32768.0 / 128) - self.x_bias
+                val = (gyr[1] << 8) | gyr[2]
+                self.gx += (((val - 0x10000) if (val & 0x8000) else val) * self.gRes - self.gx_bias)
+                val = (gyr[3] << 8) | gyr[4]
+                self.gy += (((val - 0x10000) if (val & 0x8000) else val) * self.gRes - self.gy_bias)
+                val = (gyr[5] << 8) | gyr[6]
+                self.gz += (((val - 0x10000) if (val & 0x8000) else val) * self.gRes - self.gz_bias)
                 i += 8
                 
     
@@ -102,10 +161,12 @@ class ICM42605:
         _thread.start_new_thread(self.thread_func, ())
         
     def thread_func(self):
+        import time
+        start = time.ticks_ms()
         while True:
-            #self.x += self.get_gyro_x()
-            self.read_fifo()
-            time.sleep_ms(1)
+            self.updateData(time.ticks_diff(time.ticks_ms(), start))
+            start = time.ticks_ms()
+            time.sleep_ms(50)
             
             
 class BMP388:
